27e8788a9aa32e16bc7acb8a0acb045bfa80f9f5
Jay Wen <jaywen89@gmail.com>
2019-11-17 18:31:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,6 @@ def monopoly_with_third_tier
     monopoly = {
     :railroads=> {
       pieces: 4,
-                {
                   rent_in_dollars:
                   {one_piece_owned: 25, 
                   two_pieces_owned: 50, 
