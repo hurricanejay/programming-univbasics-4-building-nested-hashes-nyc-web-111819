668bada5f48071ee925d4fa8abf36a234a33eb21
Jay Wen <jaywen89@gmail.com>
2019-11-17 18:34:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,6 +28,7 @@ def monopoly_with_third_tier
         :reading_railroad => {}
           :pennsylvania_road => {}
           :b_and_o_railroad => {}
+          :shortline_railroad => {}
         }
       }
                   rent_in_dollars:
